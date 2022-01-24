2f137edd6ec25666c47af0b3e61dd494c913fa88
taek <yoda3708@gmail.com>
2022-01-24 20:34:21 +0900

    add : headerStyle

--- react_navigation_01/App.js
+++ react_navigation_01/App.js
@@ -7,21 +7,68 @@
  */
 
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './src/home'
 import UserScreen from './src/user'
+import LogoTitle from './src/logo'
 
 const Stack = createNativeStackNavigator()
 
 class App extends Component {
+  logoTitle = () => {
+    return (
+      <Image
+        style={{ width: 40, height: 40 }}
+        source={require('./src/assets/pics/home_icon.png')}
+      />
+    )
+  }
+
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="User" component={UserScreen} />
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#a4511e'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              color: '#f3d612'
+            }
+          }}>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              title: 'Home Screen',
+              headerTitle: <LogoTitle />
+            }}
+          />
+          <Stack.Screen
+            name="User"
+            component={UserScreen}
+            initialParams={{
+              userIdx: 50,
+              userName: 'Gildong',
+              userLastName: 'Go'
+            }}
+            options={{
+              title: 'User Screen',
+              headerStyle: {
+                backgroundColor: 'pink'
+              },
+              headerTintColor: 'red',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                color: 'purple'
+              }
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     )
--- react_navigation_01/src/user.js
+++ react_navigation_01/src/user.js
@@ -10,7 +10,23 @@ import React, { Component } from 'react'
 import { View, Text, Button } from 'react-native'
 
 class UserScreen extends Component {
+  headerStyle = () => {
+    this.props.navigation.setOptions({
+      title: 'Customizing',
+      headerStyle: {
+        backgroundColor: 'blue'
+      },
+      headerTintColor: 'yellow',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        color: 'green'
+      },
+      headerBackTitle: 'BACK'
+    })
+  }
+
   render() {
+    this.headerStyle()
     const { params } = this.props.route
     const userIdx = params ? params.userIdx : null
     const userName = params ? params.userName : null
