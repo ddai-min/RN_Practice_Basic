1f2a69eea183bafed00655c8c3da6014a04c744c
taek <yoda3708@gmail.com>
2022-01-24 19:05:18 +0900

    add : stackNavigation

--- react_navigation_01/App.js
+++ react_navigation_01/App.js
@@ -9,16 +9,19 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './src/home'
+import UserScreen from './src/user'
 
-const Stack = createStackNavigator()
+const Stack = createNativeStackNavigator()
 
 class App extends Component {
   render() {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="User" component={UserScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     )
---
+++ react_navigation_01/src/home.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+    )
+  }
+}
+
+export default HomeScreen
