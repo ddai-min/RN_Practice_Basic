0b17a4dabb27da791600b2c5c9272f5ddd28d926
taek <yoda3708@gmail.com>
2022-01-27 19:04:04 +0900

    add : imagePicker

--- RNative_ImagePicker/App.js
+++ RNative_ImagePicker/App.js
@@ -11,10 +11,32 @@ import { StyleSheet, View, Text, Image, Button } from 'react-native'
 import ImagePicker from 'react-native-image-picker'
 
 class App extends Component {
+  state = {
+    avatar: ''
+  }
+
+  addImage = () => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Choose your photo',
+        takePhotoButtonTitle: 'Take a pretty one',
+        chooseFromLibraryButtonTitle: 'Select an old one',
+        cancelButtonTitle: 'Just go back'
+      },
+      response => {
+        this.setState({
+          avatar: response.uri
+        })
+      }
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <Image source={{ uri: this.state.avatar }} style={styles.avatar} />
+
+        <Button title="Add an Image" onPress={() => this.addImage()} />
       </View>
     )
   }
@@ -26,6 +48,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#e4ab26'
+  },
+  avatar: {
+    width: '100%',
+    height: 400
   }
 })
 
