c8ed95ea564daaa4bdc8082a75d509520d54d891
taek <yoda3708@gmail.com>
2022-02-07 16:29:29 +0900

    add : reuseComponent

--- RN_Animation/App.js
+++ RN_Animation/App.js
@@ -8,14 +8,17 @@
 
 import React, { Component } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
-import AnimOne from './src/Animation01'
-import AnimTwo from './src/Animation02'
+// import AnimOne from './src/Animation01'
+// import AnimTwo from './src/Animation02'
+import Supertext from './src/utils/supertext'
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <AnimTwo />
+        <Supertext style={{ backgroundColor: 'red' }}>
+          This is my template!!!
+        </Supertext>
       </View>
     )
   }
@@ -24,8 +27,9 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 50
+    backgroundColor: '#bbb',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
---
+++ RN_Animation/src/utils/supertext.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react'
+import { StyleSheet, View, Text, Button } from 'react-native'
+
+const Supertext = props => {
+  return (
+    <Text
+      // style={styles.supertext}
+      // {...props}
+      style={[styles.supertext, props.style]}>
+      {props.children}
+    </Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  supertext: {
+    backgroundColor: 'skyblue',
+    fontSize: 25,
+    color: 'blue',
+    padding: 15,
+    width: 300
+  }
+})
+
+export default Supertext
